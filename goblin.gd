d709c76f47faae0618d634bc4661ec6b1630adb3
Luo Jiale <luojiale@u.nus.edu>
2025-02-12 14:34:28 +0800

    Add enemy FSM

--- goblin.gd
+++ goblin.gd
@@ -1,15 +1,26 @@
 extends CharacterBody2D
 
 const MAX_HEALTH = 100
+const KNOCK_BACK_DISTANCE = 50
+const damage = 10
+const ATTACK_COOLDOWN_SECONDS = 0.5
+const ATTACK_CAST_DELAY_SECONDS = 0.5
+
+enum GOBLIN_STATE { IDLE, RUN, ATTACK_COOLDOWN, ATTACK_CASTING, ATTACKING, HURT }
 
 @onready var SPEED = randi_range(70, 130)
 @onready var health = MAX_HEALTH
+@onready var state = GOBLIN_STATE.IDLE : set = set_state
 
 @onready var player : CharacterBody2D = get_parent().get_node("Player")
 @onready var navigation_agent = $NavigationAgent2D
 @onready var target
 @onready var sprite = $Sprite
 @onready var animation_player = $AnimationPlayer
+@onready var attack_cooldown_timer = $AttackCooldownTimer
+@onready var attack_cast_delay_timer = $AttackCastDelayTimer
+@onready var attack_collision = $Attack_hitbox/CollisionPolygon2D
+
 
 func _ready():
 	call_deferred("navigation_setup")
@@ -17,31 +28,39 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	if health <= 0:
 		die()
-	
+		
 	follow_player()
-	
+		
 	if navigation_agent.is_navigation_finished():
-		sprite.play("idle")
-		return
-	
-	sprite.play("run")
-	
-	var next_position = navigation_agent.get_next_path_position()
-	var direction = global_position.direction_to(next_position)
-	
-	var new_velocity = direction * SPEED
-	
-	if navigation_agent.avoidance_enabled:
-		navigation_agent.velocity = new_velocity
+		if state == GOBLIN_STATE.RUN:
+			state = GOBLIN_STATE.IDLE
 	else:
-		velocity = new_velocity
+		if state == GOBLIN_STATE.IDLE:
+			state = GOBLIN_STATE.RUN
+	
+	if state == GOBLIN_STATE.RUN:
+		var next_position = navigation_agent.get_next_path_position()
+		var direction = global_position.direction_to(next_position)
+		
+		var new_velocity = direction * SPEED
+		
+		if navigation_agent.avoidance_enabled:
+			navigation_agent.velocity = new_velocity
+		else:
+			velocity = new_velocity
 
-	if velocity.x < 0:
-		sprite.flip_h = true
+		if velocity.x < 0:
+			sprite.flip_h = true
+			attack_collision.scale.x = -1
+		else:
+			sprite.flip_h = false
+			attack_collision.scale.x = 1
+	
 	else:
-		sprite.flip_h = false
-
+		velocity = lerp(velocity, Vector2(0, 0), 0.1)
+	
 	move_and_slide()
+	
 
 func navigation_setup():
 	target = player
@@ -52,9 +71,11 @@ func follow_player():
 		navigation_agent.target_position = target.global_position
 
 
-func take_damage(damage):
+func take_damage(damage, source: Node2D):
 	health = max(health - damage, 0)
 	animation_player.play("hurt")
+	state = GOBLIN_STATE.HURT
+	velocity = source.position.direction_to(position) * KNOCK_BACK_DISTANCE
 	print(health)
 	
 
@@ -62,5 +83,54 @@ func die():
 	queue_free()
 
 
+func set_state(new_state: GOBLIN_STATE):
+	var prev_state = state
+	state = new_state
+	
+	if prev_state == GOBLIN_STATE.ATTACKING:
+		attack_collision.disabled = true
+		
+	
+	match new_state:
+		GOBLIN_STATE.IDLE:
+			sprite.play("idle")
+			attack_cooldown_timer.start(ATTACK_COOLDOWN_SECONDS)
+		
+		GOBLIN_STATE.RUN:
+			sprite.play("run")
+		
+		GOBLIN_STATE.ATTACK_COOLDOWN:
+			sprite.play("idle")
+		
+		GOBLIN_STATE.ATTACK_CASTING:
+			sprite.play("attack_casting")
+			attack_cast_delay_timer.start(ATTACK_CAST_DELAY_SECONDS)
+		
+		GOBLIN_STATE.ATTACKING:
+			sprite.play("attacking")
+			attack_collision.disabled = false
+		
+		GOBLIN_STATE.HURT:
+			sprite.play("hurt")
+
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
-	velocity = safe_velocity
+	if state == GOBLIN_STATE.RUN:
+		velocity = safe_velocity
+
+
+func _on_sprite_animation_finished() -> void:
+	if state in [GOBLIN_STATE.ATTACKING, GOBLIN_STATE.HURT]:
+		state = GOBLIN_STATE.IDLE
+
+func _on_attack_cooldown_timer_timeout() -> void:
+	if state == GOBLIN_STATE.IDLE:
+		state = GOBLIN_STATE.ATTACK_CASTING
+
+func _on_attack_cast_delay_timer_timeout() -> void:
+	if state == GOBLIN_STATE.ATTACK_CASTING:
+		state = GOBLIN_STATE.ATTACKING
+
+
+func _on_attack_hitbox_body_entered(body: Node2D) -> void:
+	if "player" in body.get_groups():
+		body.take_damage(damage, self)
--- player.gd
+++ player.gd
@@ -1,48 +1,54 @@
 extends CharacterBody2D
 
+const MAX_HEALTH = 100
+const KNOCK_BACK_DISTANCE = 200
 const SPEED = 300.0
-const ATTACK_COMMON_DAMAGE = 10
+const ATTACK_1_DAMAGE = 5
+const ATTACK_2_DAMAGE = 10
 const ATTACK_3_DAMAGE = 20
-# preserve prev attack state to facilitate combo attack
-const PREV_ATTACK_STATE_RETENTION_SECONDS = 0.2 # before cleared
 
-enum PLAYER_STATE { IDLE, RUN, ATTACK_1, ATTACK_2, ATTACK_3 }
+enum PLAYER_STATE { IDLE, RUN, ATTACK_1, ATTACK_2, ATTACK_3, HURT }
 
 @onready var state: PLAYER_STATE = PLAYER_STATE.IDLE : set = set_state
 
 var prev_attack_state: PLAYER_STATE
 
+@onready var health = MAX_HEALTH
 @onready var sprite = $Sprite
+@onready var animation_player = $AnimationPlayer
 @onready var attack1_collision = $Attack1_Hitbox/CollisionPolygon2D
 @onready var attack2_collision = $Attack2_Hitbox/CollisionPolygon2D
 @onready var attack3_collision = $Attack3_Hitbox/CollisionPolygon2D
+# preserve prev attack state to facilitate combo attack
+@onready var prev_attack_state_timer = $prev_attack_state_timer
 
 
 func _physics_process(delta: float) -> void:
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
 	
-	if state in [PLAYER_STATE.IDLE, PLAYER_STATE.RUN]:
-		var direction_x := Input.get_axis("ui_left", "ui_right")
-		var direction_y = Input.get_axis("ui_up", "ui_down")
+	if state == PLAYER_STATE.IDLE and (direction_x or direction_y):
+		state = PLAYER_STATE.RUN
+	elif state == PLAYER_STATE.RUN and not (direction_x or direction_y):
+		state = PLAYER_STATE.IDLE
 		
-		if not direction_x and not direction_y:
-			state = PLAYER_STATE.IDLE
-		else:
-			velocity = Vector2(direction_x, direction_y) * SPEED
-			state = PLAYER_STATE.RUN
+	if state == PLAYER_STATE.RUN:
+		velocity = Vector2(direction_x, direction_y) * SPEED
+		state = PLAYER_STATE.RUN
+		
+		if direction_x < 0:
+			sprite.flip_h = true
+			attack1_collision.scale.x = -1
+			attack2_collision.scale.x = -1
+			attack3_collision.scale.x = -1
 			
-			if direction_x < 0:
-				sprite.flip_h = true
-				attack1_collision.scale.x = -1
-				attack2_collision.scale.x = -1
-				attack3_collision.scale.x = -1
-				
-			elif direction_x > 0:
-				sprite.flip_h = false
-				attack1_collision.scale.x = 1
-				attack2_collision.scale.x = 1
-				attack3_collision.scale.x = 1
+		elif direction_x > 0:
+			sprite.flip_h = false
+			attack1_collision.scale.x = 1
+			attack2_collision.scale.x = 1
+			attack3_collision.scale.x = 1
 	else:
-		velocity = lerp(velocity, Vector2.ZERO, 0.05)
+		velocity = lerp(velocity, Vector2.ZERO, 0.2)
 	
 	if Input.is_action_just_pressed("attack"):
 		if state not in [PLAYER_STATE.IDLE, PLAYER_STATE.RUN]:
@@ -57,23 +63,38 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_collide(velocity * delta)
 
+func take_damage(damage, source: Node2D):
+	health = max(health - damage, 0)
+	velocity = source.position.direction_to(position) * KNOCK_BACK_DISTANCE
+	animation_player.play("hurt")
+	state = PLAYER_STATE.HURT
+	
+	
 func set_state(new_state: PLAYER_STATE):
+	var prev_state = state
 	state = new_state
+	
+	match prev_state:
+		PLAYER_STATE.ATTACK_1:
+			attack1_collision.disabled = true
+			record_temp_prev_attack_state(prev_state)
+		PLAYER_STATE.ATTACK_2:
+			attack2_collision.disabled = true
+			record_temp_prev_attack_state(prev_state)
+			
+		PLAYER_STATE.ATTACK_3:
+			attack3_collision.disabled = true
+			record_temp_prev_attack_state(prev_state)
+			
 	
 	# events that happen once during state change
 	match new_state:
 		PLAYER_STATE.IDLE:
 			velocity = Vector2.ZERO
 			sprite.play("idle")
-			attack1_collision.disabled = true
-			attack2_collision.disabled = true
-			attack3_collision.disabled = true
 		
 		PLAYER_STATE.RUN:
 			sprite.play("run")
-			attack1_collision.disabled = true
-			attack2_collision.disabled = true
-			attack3_collision.disabled = true
 		
 		PLAYER_STATE.ATTACK_1:
 			sprite.play("attack_1")
@@ -86,34 +107,36 @@ func set_state(new_state: PLAYER_STATE):
 		PLAYER_STATE.ATTACK_3:
 			sprite.play("attack_3")
 			attack3_collision.disabled = false
+			
+		PLAYER_STATE.HURT:
+			sprite.play("hurt")
 
+func record_temp_prev_attack_state(state: PLAYER_STATE):
+	prev_attack_state = state
+	prev_attack_state_timer.start()
 
 func clear_prev_attack_state():
 	prev_attack_state = PLAYER_STATE.IDLE
 
 func _on_sprite_animation_finished() -> void:
-	if state in [PLAYER_STATE.ATTACK_1, PLAYER_STATE.ATTACK_2, PLAYER_STATE.ATTACK_3]:
-		prev_attack_state = state
-		var timer = Timer.new()
-		add_child(timer)
-		timer.wait_time = PREV_ATTACK_STATE_RETENTION_SECONDS
-		timer.one_shot = true
-		timer.connect("timeout", clear_prev_attack_state)
-		timer.start()
-		
+	if state in [PLAYER_STATE.ATTACK_1, PLAYER_STATE.ATTACK_2,
+				PLAYER_STATE.ATTACK_3, PLAYER_STATE.HURT]:
 		state = PLAYER_STATE.IDLE
-
 
 func _on_attack_1_hitbox_body_entered(body: Node2D) -> void:
 	if "enemy" in body.get_groups():
-		body.take_damage(ATTACK_COMMON_DAMAGE)
+		body.take_damage(ATTACK_1_DAMAGE, self)
 
 
 func _on_attack_2_hitbox_body_entered(body: Node2D) -> void:
 	if "enemy" in body.get_groups():
-		body.take_damage(ATTACK_COMMON_DAMAGE)
+		body.take_damage(ATTACK_2_DAMAGE, self)
 
 
 func _on_attack_3_hitbox_body_entered(body: Node2D) -> void:
 	if "enemy" in body.get_groups():
-		body.take_damage(ATTACK_3_DAMAGE)
+		body.take_damage(ATTACK_3_DAMAGE, self)
+
+
+func _on_prev_attack_state_timer_timeout() -> void:
+	clear_prev_attack_state()
